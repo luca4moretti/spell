3ed8a75f9de09cacdb0e5c9c52c012c7e753dac2
luca4moretti <luca4moretti@proton.me>
2026-07-25 18:34:43 +0300

    Create sample_movies.py

---
+++ data/sample_movies.py
@@ -0,0 +1,31 @@
+MOVIES = [
+
+    {
+        "title": "The Prestige",
+        "genre": "Drama",
+        "rating": 5,
+        "watched": True
+    },
+
+    {
+        "title": "Arrival",
+        "genre": "Sci-Fi",
+        "rating": 5,
+        "watched": True
+    },
+
+    {
+        "title": "Interstellar",
+        "genre": "Sci-Fi",
+        "rating": 5,
+        "watched": False
+    },
+
+    {
+        "title": "The Lighthouse",
+        "genre": "Drama",
+        "rating": 4,
+        "watched": True
+    }
+
+]
