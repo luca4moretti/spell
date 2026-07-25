3c4b268cbdb46c2a31ec248ce3b5c66684d475d7
luca4moretti <luca4moretti@proton.me>
2026-07-25 19:05:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
-""" Movie watchlist app """
+from config import EXPORT_FILE
+from config import MIN_RATING
 
-print("Project: movie-watchlist")
+from repository.movie_repository import MovieRepository
+
+from services.statistics_service import StatisticsService
+from services.recommendation_service import RecommendationService
+
+from renderer.console_renderer import ConsoleRenderer
+
+from exporters.json_exporter import JsonExporter
+
+movies = MovieRepository().load()
+
+recommended = RecommendationService().recommend(
+
+    movies,
+
+    MIN_RATING
+
+)
+
+stats = StatisticsService().build(
+
+    movies
+
+)
+
+ConsoleRenderer().display(
+
+    recommended,
+
+    stats
+
+)
+
+JsonExporter().export(
+
+    movies,
+
+    EXPORT_FILE
+
+)
